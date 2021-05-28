658b2453d01464496d2bce7c9b54218d86821df3
github username <github email address>
2021-05-27 23:51:25 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,11 +1,17 @@
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+      debugger
+      const band = {
+        id: state.bands.length,
+        name: action.payload
+      }
+      return { ...state, bands: [...state.bands, band] }
+    case 'DELETE_BAND':
+      return {bands: state.bands.filter(b => b.id !== action.id)}
     default:
       return state;
   }
